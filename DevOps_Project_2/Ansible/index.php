5e65a9aac356f78d0247e5bbc68dfe23ea3e3fe3
BADAVATH SRIKANTH <80474350+SRIKANTH284@users.noreply.github.com>
2023-03-19 15:52:21 +0530

    Delete DevOps_Project_2 directory

--- DevOps_Project_2/Ansible/index.php
+++
@@ -1,7 +0,0 @@
-<body bgcolor='white'>
-<pre>
-<?php
-print("<b><font color=\"blue\">!! Hello !! This is Euran,\n\n! Welcome ! to My Web Server</font>\n\n");
-print (`/usr/sbin/ifconfig`);
-?>
-</pre>
--- DevOps_Project_2/Terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "euran-terraform-statefile"
-    key = "server_name/statefile"
-    region = "ap-south-1"
-  }
-}  
